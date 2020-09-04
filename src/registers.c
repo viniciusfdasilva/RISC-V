156095249144d9a3db8b971961ce28cec11d157e
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2020-09-04 15:58:23 -0300

    [src] Feature - Registers Codes

---
+++ src/registers.c
@@ -0,0 +1,58 @@
+MIT License
+
+Copyright (c) 2020 Dayane Cordeiro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+/**
+ * @name Code of Registers
+ */
+
+#define REG_ZERO_NUM_STR 0X00
+#define REG_RA_NUM_STR	 0X01
+#define REG_SP_NUM_STR   0X02
+#define REG_GP_NUM_STR   0X03
+#define REG_TP_NUM_STR   0X04
+#define REG_T0_NUM_STR   0X05
+#define REG_T1_NUM_STR   0X06
+#define REG_T2_NUM_STR   0X07
+#define REG_S0_NUM_STR   0X08
+#define REG_S1_NUM_STR   0X09
+#define REG_A0_NUM_STR   0X0a
+#define REG_A1_NUM_STR   0X0b
+#define REG_A2_NUM_STR   0X0c
+#define REG_A3_NUM_STR   0X0d
+#define REG_A4_NUM_STR   0X0e
+#define REG_A5_NUM_STR   0X0f
+#define REG_A6_NUM_STR   0X10
+#define REG_A7_NUM_STR   0X11
+#define REG_S2_NUM_STR   0X12
+#define REG_S3_NUM_STR   0X13
+#define REG_S4_NUM_STR   0X14
+#define REG_S5_NUM_STR   0X15
+#define REG_S6_NUM_STR   0X16
+#define REG_S7_NUM_STR   0X17
+#define REG_S8_NUM_STR   0X18
+#define REG_S9_NUM_STR   0X19
+#define REG_S10_NUM_STR  0X1a
+#define REG_S11_NUM_STR  0X1b
+#define REG_T3_NUM_STR   0X1c
+#define REG_T4_NUM_STR   0X1d
+#define REG_T5_NUM_STR   0X1e
+#define REG_T6_NUM_STR   0X2f
