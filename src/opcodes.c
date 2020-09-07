77743b25c6b49973f4acfd99b1dc050efcd27f19
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2020-09-07 16:48:15 -0300

    [src] Feature: Instructions Opcodes Division

---
+++ src/opcodes.c
@@ -0,0 +1,35 @@
+MIT License
+
+Copyright (c) 2020 Dayane Cordeiro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+/* Instructions Opcodes Division */
+
+#define U_TYPE_IMMEDIATE_iNSTRUCTION		0X37
+#define U_TYPE_PC_INSTRUCTION			0X17
+#define J_TYPE_INSTRUCTION			0X6f
+#define I_TYPE_JUMPER_INSTRUCTION		0x67
+#define I_TYPE_LOAD_INSTRUCTIONS		0x03
+#define I_TYPE_REGISTERS_INSTRUCTIONS		0x13
+#define I_TYPE_FENCE_INSTRUCTIONS		0x0f
+#define I_TYPE_CALL_BREAKPOINT_CRS_INSTRUCTIONS	0x73
+#define B_TYPE_INSTRUCTIONS			0x63
+#define S_TYPE_INSTRUCTIONS			0x23
+#define R_TYPE_INSTRUCTIONS			0x33
