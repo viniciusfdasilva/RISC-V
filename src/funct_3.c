cbccb9f52b9d97d8474ba50e4904b13399fd10ac
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2020-09-14 15:43:57 -0300

    [vmachine] Feature: I-Type Funct_3 Codes Definition

--- src/funct_3.c
+++ src/funct_3.c
@@ -23,6 +23,7 @@ SOFTWARE.
 /* Definition of instructions func_3 */
 /* DOING */
 
+/* R-Type */
 #define INST_ADD_FUNCT_3  0x00
 #define INST_SUB_FUNCT_3  0x00
 #define INST_SLL_FUNCT_3  0x01
@@ -33,3 +34,30 @@ SOFTWARE.
 #define INST_SRA_FUNCT_3  0x05
 #define INST_OR_FUNCT_3   0x06
 #define INST_AND_FUNCT_3  0x07
+
+/* I_Type */
+#define INST_JALR_FUNCT_3       0x00
+#define INST_LB_FUNCT_3         0x00
+#define INST_LH_FUNCT_3         0x01
+#define INST_LW_FUNCT_3         0x02
+#define INST_LBU_FUNCT_3        0x04
+#define INST_LHU_FUNCT_3        0x05
+#define INST_ADDI_FUNCT_3       0x00
+#define INST_SLTI_FUNCT_3       0x02
+#define INST_SLTIU_FUNCT_3      0x03
+#define INST_XORI_FUNCT_3       0x04
+#define INST_ORI_FUNCT_3        0x06
+#define INST_ANDI_FUNCT_3       0x07
+#define INST_SLLI_FUNCT_3       0x01
+#define INST_SRLI_FUNCT_3       0x05
+#define INST_SRAI_FUNCT_3       0x05
+#define INST_FENCE_FUNCT_3      0x00
+#define INST_FENCE_I_FUNCT_3    0x01
+#define INST_ECALL_FUNCT_3      0x00
+#define INST_EBREAK_FUNCT_3     0x00
+#define INST_CSRRW_FUNCT_3      0x01
+#define INST_CSRRS_FUNCT_3      0x02
+#define INST_CSRRC_FUNCT_3      0x03
+#define INST_CSRRWI_FUNCT_3     0x05
+#define INST_CSRRSI_FUNCT_3     0x06
+#define INST_CSRRCI_FUNCT_3     0x07
